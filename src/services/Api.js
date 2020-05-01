313e78b5cfdd4d59151c95dadc78b45b2e1f5cb8
Diego Santos <dsaantos1995@gmail.com>
2020-05-01 12:32:34 -0300

    resolve link to get api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const urlNow = window.location.host === 'localhost:3000' ? 'http://api.tvmaze.com/' : 'https://api.tvmaze.com/';
 
 
 const api = axios.create({
-  baseURL: 'http://api.tvmaze.com/'
+  baseURL: urlNow
   // headers: {'X-Custom-Header': 'foobar'}
 })
 
